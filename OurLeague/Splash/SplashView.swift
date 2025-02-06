f7368cee055cc3e780d6b0e0ce3683dd5209a6c5
yanghojoon <yanghojoon8487@gmail.com>
2025-02-06 18:54:32 +0900

    feat: Splash 뷰 추가

---
+++ OurLeague/Splash/SplashView.swift
@@ -0,0 +1,39 @@
+//
+//  SplashView.swift
+//  OurLeague
+//
+//  Created by 양호준 on 2/6/25.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    @State private var navigateToNextPage = false
+
+    var body: some View {
+        ZStack {
+            LinearGradient(
+                gradient: Gradient(colors: [Color.green, Color.yellow]),
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            )
+            Image(systemName: "tennis.racket.circle")
+                .resizable()
+                .frame(width: 100, height: 100)
+            if navigateToNextPage {
+                HomeView()
+                    .transition(.opacity)
+            }
+        }
+        .ignoresSafeArea()
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(3)) {
+                navigateToNextPage = true
+            }
+        }
+    }
+}
+
+#Preview {
+    SplashView()
+}
