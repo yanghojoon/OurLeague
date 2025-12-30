50c762f2a4fde2c144b19ea13f1c9a984f6073ba
yanghojoon <yanghojoon8487@gmail.com>
2025-12-30 23:28:36 +0900

    feat: 참가자 입력 뷰 추가

--- OurLeague/GameMaker/GameMakerView.swift
+++ OurLeague/GameMaker/GameMakerView.swift
@@ -8,16 +8,20 @@
 import SwiftUI
 
 struct GameMakerView: View {
-    @State private var courtCount: Int = 2
+    @State private var courtCount: Int = 1
     @State private var gameCount: Int = 1
+    @State private var participants: [String] = []
+    @State private var playerName: String = ""
+    @State private var isGameStarted = false
+    @FocusState private var isFocused: Bool
     
     var body: some View {
-        NavigationView {
-            ZStack {
-                backgroundView()
-                    .ignoresSafeArea()
-            }
-            .navigationBarHidden(true)
+        ZStack {
+            backgroundView()
+                .ignoresSafeArea()
+        }
+        .onTapGesture {
+            isFocused = false
         }
     }
 }
@@ -61,7 +65,7 @@ extension GameMakerView {
             }
             .padding(.top, topPadding)
         }
-
+        
     }
     
     private func contentView() -> some View {
@@ -77,31 +81,35 @@ extension GameMakerView {
                 endPoint: .trailing
             )
             
-            // 메인 콘텐츠 (스크롤 가능)
-            ScrollView {
-                // 이전에 만들어두신 courtCountView를 스크롤 뷰 안으로 옮겨 재사용할 수 있습니다.
-                courtCountView()
-                    .clipShape(RoundedRectangle(cornerRadius: 12))
-                    .shadow(color: .gray.opacity(0.4), radius: 5, x: 4, y: 4)
-                    .padding(.horizontal)
-                    .padding(.top, 20)
-                
-                gameCountView()
-                    .clipShape(RoundedRectangle(cornerRadius: 12))
-                    .shadow(color: .gray.opacity(0.4), radius: 5, x: 4, y: 4)
-                    .padding(.horizontal)
-                    .padding(.top, 8)
-                // TODO: 게임 수, 선수 명단 등 다른 입력 UI를 여기에 추가합니다.
+            ScrollViewReader { scrollProxy in
+                ScrollView {
+                    participantInputView()
+                        .clipShape(RoundedRectangle(cornerRadius: 12))
+                        .shadow(color: .gray.opacity(0.4), radius: 5, x: 4, y: 4)
+                        .padding(.horizontal)
+                        .padding(.top, 20)
+                    
+                    courtCountView()
+                        .clipShape(RoundedRectangle(cornerRadius: 12))
+                        .shadow(color: .gray.opacity(0.4), radius: 5, x: 4, y: 4)
+                        .padding(.horizontal)
+                        .padding(.top, 8)
+                    gameCountView()
+                        .clipShape(RoundedRectangle(cornerRadius: 12))
+                        .shadow(color: .gray.opacity(0.4), radius: 5, x: 4, y: 4)
+                        .padding(.horizontal)
+                        .padding(.top, 8)
+                }
+                .scrollBounceBehavior(.basedOnSize, axes: .vertical)
+                .padding(.bottom)
             }
-            .scrollBounceBehavior(.basedOnSize, axes: .vertical)
-            .padding(.bottom, 100)
-
-            // 하단 플로팅 바
+            
             VStack {
                 Spacer()
                 floatingBottomBar()
             }
-            .ignoresSafeArea(edges: .bottom)
+            .ignoresSafeArea(.keyboard)
+            
         }
     }
     
@@ -109,9 +117,9 @@ extension GameMakerView {
         VStack(spacing: 0) {
             HStack(spacing: 12) {
                 Button(action: {
-                    courtCount = 2
+                    courtCount = 1
                     gameCount = 1
-                    print("이전 버튼 탭")
+                    participants = []
                 }) {
                     Text("초기화")
                         .font(.headline)
@@ -125,19 +133,11 @@ extension GameMakerView {
                             RoundedRectangle(cornerRadius: 8)
                                 .stroke(Color.gray, lineWidth: 1)
                         }
-//                    HStack {
-//                        Image(systemName: "gobackward")
-//                            .font(.system(size: 10))
-//                        Text("초기화")
-//                            .font(.headline)
-//                    }
-//                    .foregroundStyle(Color.black)
-//                    .frame(minWidth: .infinity)
-//                    .padding()
-//                    .contentShape(RoundedRectangle(cornerRadius: 8))
                 }
                 
-                NavigationLink(destination: Text("선수 선택 화면 (Placeholder)")) {
+                Button {
+                    isGameStarted = true
+                } label: {
                     Text("게임 생성")
                         .font(.headline)
                         .fontWeight(.semibold)
@@ -147,6 +147,7 @@ extension GameMakerView {
                         .background(Color(red: 16 / 255, green: 185 / 255, blue: 129 / 255))
                         .cornerRadius(8)
                 }
+                
             }
             .padding(.horizontal, 16)
             .padding(.top, 12)
@@ -181,7 +182,7 @@ extension GameMakerView {
                         .padding(.top, 16)
                     Spacer()
                 }
-
+                
                 Spacer()
                     .frame(height: 20)
                 Stepper {
@@ -219,7 +220,7 @@ extension GameMakerView {
                         .padding(.top, 16)
                     Spacer()
                 }
-
+                
                 Spacer()
                     .frame(height: 20)
                 Stepper {
@@ -237,7 +238,89 @@ extension GameMakerView {
             }
         }
     }
-
+    
+    private func participantInputView() -> some View {
+        ZStack {
+            Color.white
+            VStack(alignment: .leading) { // 정렬 추가
+                HStack {
+                    Image(systemName: "person")
+                        .font(.system(size: 12))
+                        .frame(width: 25, height: 25)
+                        .foregroundStyle(Color(red: 20 / 255, green: 140 / 255, blue: 30 / 255))
+                        .background(Color.green.opacity(0.2))
+                        .clipShape(RoundedRectangle(cornerRadius: 4))
+                    
+                    Text("참가자 (\(participants.count)명)")
+                        .font(.subheadline)
+                        .fontWeight(.bold)
+                    
+                    Spacer()
+                }
+                .padding(.leading, 20)
+                .padding(.top, 16)
+                
+                if !participants.isEmpty {
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack {
+                            ForEach(participants, id: \.self) { player in
+                                HStack {
+                                    Text(player)
+                                        .font(.caption)
+                                    Image(systemName: "xmark")
+                                        .font(.system(size: 8))
+                                        .frame(width: 10, height: 10)
+                                }
+                                .padding(.horizontal, 10)
+                                .padding(.vertical, 4)
+                                .background(Color.gray.opacity(0.1))
+                                .cornerRadius(16)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 16)
+                                        .stroke(Color.gray.opacity(0.5), lineWidth: 1)
+                                )
+                                .onTapGesture {
+                                    if let index = participants.firstIndex(of: player) {
+                                        participants.remove(at: index)
+                                    }
+                                }
+                            }
+                        }
+                        .padding(.horizontal, 20)
+                    }
+                    .padding(.top, 5)
+                }
+                
+                HStack {
+                    TextField("이름 입력", text: $playerName)
+                        .font(.system(size: 14))
+                        .focused($isFocused)
+                        .textFieldStyle(.roundedBorder)
+                        .onSubmit {
+                            if !playerName.isEmpty {
+                                participants.append(playerName)
+                                playerName = ""
+                                isFocused = true
+                            }
+                        }
+                    
+                    Button {
+                        participants.append(playerName)
+                        playerName = ""
+                    } label: {
+                        Text("추가")
+                            .frame(width: 40, height: 28)
+                            .foregroundStyle(Color.white)
+                            .font(.system(size: 14))
+                            .background(Color(red: 16 / 255, green: 185 / 255, blue: 129 / 255))
+                            .cornerRadius(8)
+                    }
+                }
+                .padding(.horizontal, 20)
+                .padding(.bottom, 16)
+            }
+        }
+    }
 }
 
 #Preview {
